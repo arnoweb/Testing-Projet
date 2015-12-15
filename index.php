e293bd509b244a4ebacc2d9cdae72b929bbd589e
Arnaud BRETON <a.breton@mazarinedigital.com>
2015-12-15 15:52:35 +0100

    Master changes 8

--- index.php
+++ index.php
@@ -3,7 +3,4 @@ echo "this is a test which fix the master branch";
 echo "this is a test which fix the master branch 2";
 echo "this is a test which fix the master branch 3";
 echo "this is a test which fix the master branch 4";
-echo "this is a test which fix the master branch 5";
-echo "this is a test which fix the master branch 6";
-echo "this is a test which fix the master branch 7";
 ?>
