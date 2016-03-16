6b68624a294bf2d35673d35cc1396ff2fce9a371
Arnoweb <arnbret2004n@yahoo.fr>
2016-03-16 23:18:36 +0100

    Revert "ticket 1"
    This reverts commit 98126a79a4a3a3c5a3b1988c6ba1fe5729b58668.
    
    # Conflicts:
    #	index.php

--- index.php
+++ index.php
@@ -1,2 +1,9 @@
 <?php
-echo "test 3";
+echo "this is a test which fix the master branch 6";
+echo "un stash n'est pas commité";
+echo "ceci vient de la branche develop";
+echo "ceci est un travail en attente";
+echo "ceci est un travail en attente 2";
+echo "ceci est un travail en attente 3";
+echo "ceci est un travail en attente 4";
+echo "ceci est un travail en attente 111";
