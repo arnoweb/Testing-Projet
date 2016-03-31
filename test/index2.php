f481bd8309f5d99361e919a99d44e080dd0c19f5
Arnoweb <arnbret2004n@yahoo.fr>
2016-03-31 17:01:58 +0200

    ticket 36

--- test/index2.php
+++ test/index2.php
@@ -1,5 +1,5 @@
 <?php
 echo "This s a new message for hotfix 2";
 echo "This s a new message for hotfix 3";
-echo "test commit 5 cccc ddd ffff zzz tttt";
+echo "test commit 5 cccc ddd ffff zzz tttt mmmm";
 ?>
